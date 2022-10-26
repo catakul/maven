d66066b9b7843f81f7303a4ed3c25560c16db09f
Cengizhan Atakul <cengizatakul@hotmail.de>
2022-10-26 16:37:32 +0200

    Initial commit

---
+++ src/main/java/de/neuefische/Main.java
@@ -0,0 +1,17 @@
+package de.neuefische;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static int addierren(int a, int b) {
+
+        int result;
+        result=a+b;
+
+        return result;
+    };
+
+}
---
+++ src/test/java/de/neuefische/MainTest.java
@@ -0,0 +1,18 @@
+package de.neuefische;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    public void Addition() {
+        int sum = 4+5;
+
+
+        int result = de.neuefische.Main.addierren(4, 5);
+        assertEquals(sum, result  );
+    }
+
+}
